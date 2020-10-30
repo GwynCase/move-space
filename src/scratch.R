c73e49667dae3d145d436bc557243e79c6c0c6f6
Gwyn Case <45114561+GwynCase@users.noreply.github.com>
2020-10-30 12:24:10 -0700

    nest time

--- src/scratch.R
+++ src/scratch.R
@@ -1,18 +1,16 @@
 
-ggplot(visit.stats, aes(x=timeSinceLastVisit)) +
-  geom_density() +
-  theme_classic()
-
-head(visit.stats)
-
-ggplot(visit.stats, aes(x=time, y=timeSinceLastVisit)) +
-  geom_boxplot() +
-  theme_classic() +
-  geom_jitter(alpha=0.25) +
-  labs(x='entrance time', y='visit duration')
-
-foraging.stats %>% drop_na(timeSinceLastVisit)
-
-ggplot(visit.stats, aes(x=timeSinceLastVisit, y=timeInside)) +
+ggplot(visit.stats, aes(x=date, y=timeSinceLastVisit)) +
   geom_point() +
-  theme_classic()
+  theme_classic() +
+  facet_wrap(~id, scales='free')
+
+ggplot(visit.stats, aes(x=date, y=timeInside)) +
+  geom_point() +
+  theme_classic() +
+  facet_wrap(~id, scales='free')
+
+ggplot(visit.stats, aes(x=time)) +
+  geom_histogram(stat='count') +
+  theme_classic() +
+  facet_wrap(~id)
+
