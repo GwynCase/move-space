0426d6aa8e2ae069926d4dee4a90074d5805a8af
Gwyn Case <45114561+GwynCase@users.noreply.github.com>
2021-05-21 12:16:11 -0700

    catchup

--- src/scratch.R
+++ src/scratch.R
@@ -1,3 +1,13 @@
 df %>% mutate(xcoord = unlist(map(df$geometry,1)),
                     ycoord = unlist(map(df$geometry,2))) %>%
-  data.frame() %>% view()
+  data.frame() %>% class()
+
+test <- df %>% mutate(xcoord = unlist(map(df$geometry,1)),
+                      ycoord = unlist(map(df$geometry,2))) %>%
+  data.frame()
+
+test %>% class()
+class(df)
+
+test %>% st_as_sf(coords='geometry')
+class(test$geometry)
